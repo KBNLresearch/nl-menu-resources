73dc8a69dfcda88e8676d560655e849e15e6b1b8
bitsgalore <johan.vanderknijff@kb.nl>
2018-04-11 18:32:51 +0200

    Don't use base href

--- scripts/fixhtml.sh
+++ scripts/fixhtml.sh
@@ -2,20 +2,17 @@
 
 # Fix relative links in nl-menu site
 
-rootDir=.
+# Location on file system
+rootDir=/var/www/www.nl-menu.nl/rubbish
 
-# Base URL (string literal!)
-baseURL='"http://localhost/rubbish/"'
-#baseURL='"http://www.kbresearch.nl/nl-menu/"'
-
-# Construct base tag element
-baseTag="<base href="$baseURL" />"
+# Base directory on web server 
+baseDir=/rubbish
 
 while IFS= read -d $'\0' -r file ; do
     # write base href tag to header 
-    sed -i "s|<head>|<head>\n$baseTag|" $file
+    #sed -i "s|<head>|<head>\n$baseTag|" $file
     # Rewrite relative links
-    sed -i "s|/nlmenu.nl|nlmenu.nl|g" $file
-    sed -i "s|/nlmenu.en|nlmenu.en|g" $file
+    sed -i "s|/nlmenu.nl|$baseDir/nlmenu.nl|g" $file
+    sed -i "s|/nlmenu.en|$baseDir/nlmenu.en|g" $file
 done < <(find $rootDir -type f -regex '.*\.\(html\|shtml\)' -print0)
 
